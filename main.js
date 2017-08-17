618e0f08ecdab0dc8e890e38446a6fca19f9c3b6
dragosiordachioaia <xdragosy@gmail.com>
2017-08-17 13:54:40 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,292 @@
+/**
+  In order to specify the event/performance to load availability for, as well
+  as to specify where to load the widget, we use a configuration object.
+  Configuration parameters needed:
+    eventID
+    perfID
+    selector (for the container that will hold the widget)
+
+  In order to allow the host page to respond to user actions on the seating plan,
+  there are a suite of event listeners available:
+    onChartInitialised
+    onAddSeat
+    onRemoveSeat
+    onEmptyBasket
+    onGoToCheckout
+    onNewAvailabilityData
+    onNewLegendColors
+    onReserveStopped
+  All of the actions taken will send a payload of data necessary to display the
+  right information to the user.
+
+  There are also some imperative methods used to control the widget,
+  that we can call either before or after initialising it:
+    zoomIn
+    zoomOut
+    resetChart
+    hideControls
+    showControls
+    hideLegend
+    showLegend
+    reserve
+    release
+    changeColorScheme
+    selectPerformance
+
+  Availability data and colours are received via their own
+  callbacks (onNewAvailabilityData and onNewLegendColors) and
+  are needed in order to display pricing information and colors for the selected
+  seats. (Each seat data contains a reference to a specific legend item,
+  for price/colour)
+**/
+
+var colors = {};
+var domain = getQueryStringParam(window.location.href, 'domain');
+var username = getQueryStringParam(window.location.href, 'username');
+var password = getQueryStringParam(window.location.href, 'password');
+
+// widget configuration
+var chartConfig = {
+  eventID: getQueryStringParam(window.location.href, 'event'),
+  token: getQueryStringParam(window.location.href, 'token'),
+  selector: '#chart-container',
+  silenceWarnings: false,
+  preloaderColor: '#EC008C',
+  domain: domain,
+}
+
+var performance = getQueryStringParam(window.location.href, 'perf');
+
+if(performance) {
+  chartConfig.perfID = getQueryStringParam(window.location.href, 'perf');
+}
+
+$('button#select-perf').click(selectNewPerf);
+function selectNewPerf(event) {
+  var perfID = $('input#perf-id').val();
+  chart.selectPerformance(perfID);
+}
+
+var perfs = ['2GXJ-441','2GXJ-442','2GXJ-47J','2GXJ-441','2GXJ-442','2GXJ-47J','2GXJ-441','2GXJ-442','2GXJ-47J'];
+var crtPerf = 0;
+
+// initialising the widget
+var chart = new IngressoSeatingPlan();
+
+// subscribing to events
+chart.onAddSeat = addSeat;
+chart.onRemoveSeat = removeSeat;
+chart.onEmptyBasket = emptyBasket;
+chart.onSeatsReserved = seatsReserved;
+chart.onGoToCheckout = goToCheckout;
+chart.onNewAvailabilityData = onNewAvailabilityData;
+chart.onNewLegendColors = onNewLegendColors;
+chart.onReserveStopped = onReserveStopped;
+
+// setting a custom color scheme
+chart.changeColorScheme(['#462446', '#B05F6D', '#EB6B56', '#FFC153', '#47B39D', '#CDCBA6', '#008891', '#00587A', '#ff0000', '#0F3057']);
+
+chart.showControls();
+document.querySelector('#checkbox-controls').checked = true;
+
+chart.showLegend();
+document.querySelector('#checkbox-legend').checked = true;
+
+chart.init(chartConfig);
+
+// using custom UI elements to control the widget
+$('#zoom-controls #zoom-in').click(chart.zoomIn);
+$('#zoom-controls #zoom-out').click(chart.zoomOut);
+$('#zoom-controls #reset-chart').click(chart.resetChart);
+$('#zoom-controls #show-chart').click(chart.show);
+$('#zoom-controls #hide-chart').click(chart.hide);
+$("#basket>button#checkout").click(onCheckoutButton);
+$("#checkout-modal #release").click(releaseReservation);
+
+function addSeat(data) {
+  $('#response-json #content').html(syntaxHighlight(data));
+  $('#response-json #event-name').text("addSeat");
+  updateBasket(data.basket);
+}
+
+
+function updateBasket(newBasket) {
+  $('#basket #seats').empty();
+  newBasket.seats.forEach(function(seat) {
+    var seatElement = $("<p class='item' id='" + seat.uuid + "'> </p>");
+    seatElement.append("<span class='color-code' style='border-color: " + colors[seat.legend].selected + "; background-color:" + colors[seat.legend].normal + "'></span>")
+    seatElement.append("<span class='description'>" + availability.seat_blocks[seat.seat_block].desc + "</span>")
+    seatElement.append("<span class='price'>" + getFormattedPrice(availability.legend[seat.legend].price, availability.currency) + "</span>")
+    seatElement.append("<br>")
+    seatElement.append("<span style='margin-left: 18px;' class='id'> Seat: " + seat.seat_id + "</span>")
+    $('#basket #seats').append(seatElement);
+  })
+  if(newBasket.seats.length == 0) {
+    $("#basket #total #label").text("Your basket is currently empty");
+    $('#basket #total').removeClass("with-seats");
+    $("#basket>button#checkout").css('display','none');
+    $("#basket #total #value").css('opacity', '0');
+  } else {
+    $("#basket #total #value").css('opacity', '1');
+    $("#basket #total #value").text(getFormattedPrice(newBasket.total, availability.currency));
+    $("#basket #total #label").text('Total:');
+    $('#basket #total').removeClass("with-seats");
+    $('#basket #total').addClass("with-seats");
+    $("#basket>button#checkout").css('display','block');
+  }
+}
+
+function removeSeat(data) {
+  $('#response-json #event-name').text("removeSeat");
+  $('#response-json #content').html(syntaxHighlight(data));
+  updateBasket(data.basket);
+}
+function emptyBasket(data) {
+  $('#response-json #event-name').text("emptyBasket");
+  $('#response-json #content').html(syntaxHighlight(data));
+  updateBasket(data.basket);
+}
+
+
+function onNewAvailabilityData(data) {
+  availability = data.availability;
+}
+
+function onNewLegendColors(data) {
+  colors = data.colors;
+}
+
+function onReserveStopped() {
+  $('button#checkout .preloader').hide();
+  $('button#checkout .text').show();
+  $('button#checkout').removeClass('disabled');
+}
+
+function goToCheckout(data) {
+  $('#response-json #content').html(syntaxHighlight(data));
+  $('#response-json #event-name').text("goToCheckout");
+  $('button#checkout .preloader').hide();
+  $('button#checkout .text').show();
+  $('button#checkout').removeClass('disabled');
+  $("#checkout-modal").modal('show');
+  $('#checkout-modal #total').html($('#basket #total').html());
+  $('#checkout-modal .transaction-uuid').html('Transaction uuid: </br>' + data.transaction_uuid);
+  $('#checkout-modal #seats').html($('#basket #seats').html());
+}
+function seatsReserved(data) {
+  $('#response-json #content').html(syntaxHighlight(data.data));
+  $('#response-json #event-name').text("reserve");
+  goToCheckout(data);
+}
+
+$("#basket #total #value").css('opacity', '0');
+$("#basket #total #label").text("Your basket is currently empty");
+
+$("#checkbox-legend").change(onCheckboxLegendChanged);
+function onCheckboxLegendChanged(e) {
+  if(e.target.checked) {
+    chart.showLegend();
+  } else {
+    chart.hideLegend();
+  }
+}
+
+$("#checkbox-controls").change(onCheckboxControlsChanged);
+function onCheckboxControlsChanged(e) {
+  if(e.target.checked) {
+    chart.showControls();
+  } else {
+    chart.hideControls();
+  }
+}
+
+$("#select-color-scheme").change(onSelectColorSchemeChanged);
+function onSelectColorSchemeChanged(e) {
+  var option = e.target.selectedIndex;
+
+  if(option == 0) { // default
+    chart.changeColorScheme(['#f95e5e', '#FFA726', '#42A5F5', '#fcabc7', '#8febf7', '#55cc5b', '#ae87f2', '#efdc32', '#018930','#a37d70', '#455A64', '#7B1FA2', '#fc9292','#aaaaaa','#fcc18a', '#66FF99']);
+  } if(option == 1) { // pastel
+    chart.changeColorScheme(['#462446', '#B05F6D', '#EB6B56', '#FFC153', '#47B39D', '#CDCBA6', '#008891', '#00587A', '#ff0000', '#0F3057']);
+  } else if (option == 2){ // bold
+    chart.changeColorScheme(['#8ea1b3', '#101415', '#ff2600', '#ed5165', '#839496', '#6ec14c', '#d7a302', '#0084cc', '#b433e1']);
+  } else if (option == 3){ // grayscale
+    chart.changeColorScheme(['#444', '#555', '#666', '#777', '#888', '#999', '#aaa', '#bbb', '#ccc', '#aaa', '#444', '#aaa', '#444', '#aaa', '#444', '#aaa']);
+  } else if(option == 4) { // sea shells
+    chart.changeColorScheme(['#f95e5e', '#071a24', '#265b76', '#60b2c2', '#dd9a8a', '#005cbb', '#c7e8f2', '#e5c7f2', '#f95e5e', '#071a24', '#265b76', '#60b2c2', '#dd9a8a', '#005cbb', '#c7e8f2', '#e5c7f2']);
+  }
+}
+
+
+function releaseReservation() {
+  chart.release();
+  $("#checkout-modal").modal('hide');
+}
+
+function onCheckoutButton() {
+  chart.reserve();
+  $('button#checkout').addClass('disabled');
+  $('button#checkout .preloader').show();
+  $('button#checkout .text').hide();
+}
+
+/**
+ These utility functions are not necessary in order to make full use
+ of the widget, but proved useful for the demo and are production-ready
+**/
+
+function getPreSymbol(currencyObj) {
+  if(!currencyObj || !currencyObj[Object.keys(currencyObj)[0]]) return "";
+  var currency = currencyObj[Object.keys(currencyObj)[0]];
+  return currency.pre_symbol;
+}
+
+function getPostSymbol(currencyObj) {
+  if(!currencyObj || !currencyObj[Object.keys(currencyObj)[0]]) return "";
+  var currency = currencyObj[Object.keys(currencyObj)[0]];
+  return currency.post_symbol;
+}
+
+function getFormattedPrice(price, currencyObj, roundIfOverHundred = false) {
+  if(!currencyObj) return;
+  var preSymbol = currencyObj.pre_symbol;
+  var postSymbol = currencyObj.post_symbol;
+  if(roundIfOverHundred && parseFloat(price) > 100) price = parseInt(price);
+  return preSymbol + parseFloat(price).toFixed(2) + postSymbol;
+}
+
+function getQueryStringParam(url, paramName) {
+	var valToReturn = null;
+	try {
+		valToReturn = url.split(paramName+'=')[1].split('&')[0];
+		if(valToReturn.indexOf('#') != -1) {
+			valToReturn = valToReturn.split('#')[0];
+		}
+	}
+	catch(err) {
+		// it will fail when the parameter doesn't exist; this is normal
+	}
+	return valToReturn;
+}
+
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
---
+++ readme.md
@@ -0,0 +1,143 @@
+# Ingresso Seating Plan Widget
+
+This tool allows you to easily build **interactive seat selection** into your website and is powered by the Ingresso API.
+
+## Demo:
+
+[Stable version](https://s3-eu-west-1.amazonaws.com/ticketswitch-dev/seating_plan_widget/stable/demo.html?domain=test.ticketswitch.com&event=7AB&perf=7AB-4)
+
+[Latest version](http://s3-eu-west-1.amazonaws.com/ticketswitch-dev/seating_plan_widget/qa/demo.html?domain=test.ticketswitch.com&event=7AB&perf=7AB-4)
+
+
+## Getting Started:
+
+1) Create an empty page
+
+2) Add a container element, where the widget will appear
+
+3) Import our library at the bottom of your page:
+[Get it here](http://s3-eu-west-1.amazonaws.com/ticketswitch-dev/seating_plan_widget/qa/wrapper.js)
+
+4) Configure the widget
+
+### The least amount of code required to get started:
+
+```html
+<!doctype html>
+
+<html>
+  <head>
+    <style>
+      #ingresso-widget {
+        width: 500px;
+        height: 500px;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div id="ingresso-widget"></div>
+
+    <script src="http://s3-eu-west-1.amazonaws.com/ticketswitch-dev/seating_plan_widget/qa/wrapper.js"></script>
+    <script>
+      var chartConfig = {
+        eventID: '7AB', // demo event
+        perfID: '7AB-4',
+        selector: '#ingresso-widget',
+        domain: 'test.ticketswitch.com', // this will get replaced by your whitelabel domain
+      }
+      var chart = new IngressoSeatingPlan();
+      chart.init(chartConfig);
+    </script>
+  </body>
+</html>
+```
+
+## Widget Configuration
+
+In order to specify the event/performance to load availability for, as well as to specify where to load the widget from, we use a configuration object.
+
+#### Required configuration parameters:
+
+* eventID (String)
+* perfID (String)
+* selector (String) (the identifier of the container that will hold the widget)
+* domain (String) (domain that Ingresso will provide you with, for your specific integration)
+
+#### Optional configuration parameters:  
+* token (String) (Authentication token, not required to view the demo, but required to purchase; read more about how to obtain this in [Authentication](#authentication)
+* silenceWarnings (Boolean, default is **false**) (if set to **false**, the widget will provide warnings regarding missing configuration and suggestions on fixing issues)
+* preloaderColor (String) (Hex color e.g #EC008C)
+
+## Getting information out of the widget
+
+In order to allow the host page to respond to user actions on the seating plan, there are a suite of callbacks available:
+
+* onChartInitialised
+* onAddSeat
+* onRemoveSeat
+* onEmptyBasket
+* onGoToCheckout
+* onNewAvailabilityData
+* onNewLegendColors
+* onReserveStopped
+
+All of the callbacks will also receive an object containing all the data required to display the right information to the user.
+
+## Controlling the widget
+
+There are also some imperative methods used to control the widget, that we can call either before or after initialising it:
+
+* zoomIn
+* zoomOut
+* resetChart
+* hideControls
+* showControls
+* hideLegend
+* showLegend
+* reserve
+* release
+* changeColorScheme (Array of strings) (Hex colors, e.g ['#462446', '#B05F6D', '#EB6B56', '#FFC153', '#47B39D', '#CDCBA6', '#008891', '#00587A', '#ff0000', '#0F3057'])
+* selectPerformance (String) (ID of desired performance, you get this from the Ingresso API; when this function is called, the widget will automatically make itself visible)
+
+Availability data and colours are received via their own callbacks (**onNewAvailabilityData** and **onNewLegendColors**) and are needed in order to display pricing information and colors for the selected seats. (Each seat's data contains a reference to a specific legend item, for price/colour)
+
+
+## Reserving Seats
+
+It's enough to call the **reserve** method on the library, which will start the reservation process. It will handle edge cases as well (if the selected seats have become unavailable in the meantime, or any error caused by the back-end) and will fire the **goToCheckout** callback when the reservation process is complete or **onReserveStopped** if an error has appeared. 
+
+When called, **goToCheckout** will contain a **transaction_uuid** property, that represents a unique identifier assigned to that specific order. You can then go on to reserve that order, based on the token, via the Ingresso API.
+
+## Authentication
+
+In order to purchase tickets, you need to obtain a username and password and a domain. After receiving those, you should:
+
+1) Replace the demo domain in the widget configuration object with the one you have received from us
+
+2) Make a request to <https://yourdomain.ticketswitch.com/api/b2b>. The response will have a custom HTTP Header, **x-b2b-token**. You can then pass this token in the configuration object of the widget, which will allow it to load the domain we have created for you, and will also allow you to purchase orders based on the **transaction_uuid** value.
+
+Each authentication token has a lifetime of 4 hours and is valid for all your users, so it is advisable that you make this call ahead of time and cache the result, in order to improve the UX of your page (as opposed to making it on each page load)
+
+## Customisation
+
+The widget supports easy customisation of the following attributes:
+
+* colors
+* fonts
+* texts
+* look & feel (as long as you provide us with the required CSS, we'll apply it for your integration only)
+
+However, all of these are changed via our internal control panel, so you have to ask us to do them for you.
+
+The only things that can be changed by your are the color scheme on the seating plan and the preloader color, which you should provide in the configuration object of the widget (see [Configuration](#widget-configuration))
+
+## Optimisations
+
+Even though the widget allows you to initialise it with a performance ID on page load, in most cases you should initialise it just with the event ID and no performance ID, then have it load in the background (If it doesn't have a performance ID when it is initialised, the widget will hide itself).
+
+In most cases, it is best to have a calendar that allows users to choose a performance, which you then pass to the widget.When this happens, the widget will already be ready and will become visible and there should be practically no delay before they see the interactive seating plan with availability loaded (the initial availability that the user sees is cached for a short time and then it updates, which makes for very short loading times).
+
+## Widget preloader
+
+The library creates, shows and hides the preloader for you, transparently.
