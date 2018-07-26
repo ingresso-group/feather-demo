3ca5b01416beeca3e07728855504ededeb77362f
dragosiordachioaia <xdragosy@gmail.com>
2018-07-26 14:25:06 +0100

    Wired in the basket

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,66 @@
 import React, { Component } from "react";
 
-import FeatherContainer from "components/feather_container";
 import Basket from "components/basket";
 import Sidebar from "components/sidebar";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      basket: {},
+    };
+
+    this.chart = null;
+
+    this.removeSeat = this.removeSeat.bind(this);
+
+    this.onAddSeat = this.onAddSeat.bind(this);
+    this.onRemoveSeat = this.onRemoveSeat.bind(this);
+    this.initFeather = this.initFeather.bind(this);
+  }
+
+  componentDidMount() {
+    this.initFeather();
+  }
+
+  initFeather() {
+    let chartConfig = {
+      eventID: "7AB",
+      perfID: "7AB-5",
+      selector: ".feather-container",
+      silenceWarnings: false,
+      preloaderColor: "#EC008C",
+      useHTTPS: true,
+      hasCustomLegend: true,
+    };
+
+    // initialising the widget
+    this.chart = new IngressoSeatingPlan();
+
+    // subscribing to events
+    this.chart.onAddSeat = this.onAddSeat;
+    this.chart.onRemoveSeat = this.onRemoveSeat;
+    // chart.onEmptyBasket = emptyBasket;
+    // chart.onSeatsReserved = seatsReserved;
+    // chart.onGoToCheckout = goToCheckout;
+    // chart.onNewAvailabilityData = onNewAvailabilityData;
+    // chart.onNewLegendColors = onNewLegendColors;
+    // chart.onReserveStopped = onReserveStopped;
+    // chart.onNewSendMethodsData = onNewSendMethodsData;
+    // chart.onEvent = onEvent;
+    this.chart.init(chartConfig);
+  }
+
+  onAddSeat(event) {
+    console.log("onAddSeat() 2 event = ", event);
+    this.setState({ basket: event.basket });
+  }
+  onRemoveSeat(event) {
+    this.setState({ basket: event.basket });
+  }
+
+  removeSeat(seatUUID) {
+    this.chart.removeSeat(seatUUID);
   }
 
   render() {
@@ -14,8 +68,8 @@ export default class App extends Component {
       <div className="main-container">
         <Sidebar />
         <div className="main-content">
-          <FeatherContainer />
-          <Basket />
+          <div className="feather-container" />
+          <Basket basket={this.state.basket} removeSeat={this.removeSeat} />
         </div>
       </div>
     );
--- src/components/basket.js
+++ src/components/basket.js
@@ -3,12 +3,49 @@ import React, { Component } from "react";
 export default class Basket extends Component {
   constructor(props) {
     super(props);
+
+    this.displaySeats = this.displaySeats.bind(this);
+  }
+
+  displaySeats() {
+    let seats = [];
+    if (!this.props.basket || !this.props.basket.seatBundles) {
+      return null;
+    }
+    this.props.basket.seatBundles.forEach(bundle => {
+      bundle.seats.forEach(seat => {
+        seats.push(
+          <li key={seat.uuid}>
+            {bundle.ticket_type_code} - {seat.seat_id}
+            <i
+              className="fa fa-close remove"
+              onClick={e => this.props.removeSeat(seat.uuid)}
+            />
+          </li>
+        );
+      });
+    });
+    return seats;
   }
 
   render() {
+    let basketIsActive = false;
+    if (
+      this.props.basket &&
+      this.props.basket.seatBundles &&
+      this.props.basket.seatBundles.length > 0
+    ) {
+      basketIsActive = true;
+    }
+
+    console.log("basket() seats: ", this.props.basket);
     return (
       <div className="basket">
-        <p className="header">Your order summary</p>
+        <p className={`header ${basketIsActive ? "active" : ""}`}>
+          <i className="fa fa-shopping-basket" />
+          Your order summary
+        </p>
+        <ul className="seats">{this.displaySeats()}</ul>
       </div>
     );
   }
