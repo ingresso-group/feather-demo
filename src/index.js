3b0cf0805003184b542a2414e73360135dd22edc
stewart morgan <stewart.morgan@accesso.com>
2022-11-02 13:22:28 +0000

    fixes to styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import Cookies from "js-cookie";
 import "./scss/main.scss";
 
@@ -8,6 +8,10 @@ import App from "./components/app";
 showApp();
 
 function showApp() {
-    let appContainer = <App />;
-    ReactDOM.render(appContainer, document.getElementsByTagName("main")[0]);
+    const root = ReactDOM.createRoot(document.getElementsByTagName("main")[0]);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
 }
